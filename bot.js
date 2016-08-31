9a6b116aa5ad70435adb11524ea3f9303146d5e1
Slypher <connorgill@live.com.au>
2016-08-31 11:43:28 +1000

    Added error logging

--- bot.js
+++ bot.js
@@ -8,41 +8,46 @@ var tools = require('./tools.js');
 var config = require('./config.json');
 var auth = require('./auth.json');
 
-var bot = new Discord.Client();
+try {
+    var bot = new Discord.Client({forceFetchUsers: true});
 
-var discord_token = auth.discord_token;
-var log_file = fs.createWriteStream(config.logDir + tools.getTimestamp().replace(/:/g, '') + '.log', { flags: 'w' });
-var log_stdout = process.stdout;
+    var discord_token = auth.discord_token;
+    var log_file = fs.createWriteStream(config.logDir + tools.getTimestamp().replace(/:/g, '') + '.log', { flags: 'w' });
+    var log_stdout = process.stdout;
+    var log_stderr = process.stderr;
 
-// setup logging to the log directory
-console.log = function (d) {
-    log_file.write(util.format(d) + '\r\n');
-    log_stdout.write(util.format(d) + '\n');
-};
+    // setup logging to the log directory
+    console.log = function (d) {
+        log_file.write(util.format(d) + '\r\n');
+        log_stdout.write(util.format(d) + '\n');
+    };
 
-exports.getBot = function () {
-    return bot;
+    exports.getBot = function () {
+        return bot;
+    }
+
+    // create directories if they don't exist
+    if (!fs.existsSync(config.fileDir)) fs.mkdirSync(config.fileDir);
+    if (!fs.existsSync(config.logDir)) fs.mkdirSync(config.logdir);
+    if (!fs.existsSync(config.serverDir)) fs.mkdirSync(config.serverDir);
+
+    // load plugins
+    fs.readdirSync(config.pluginDir).forEach(function (file) {
+        var plugin = require(config.pluginDir + file);
+        for (var i = 0; i < plugin.commands.length; i++) events.addCommand(plugin.commands[i], plugin[plugin.commands[i]]);
+        for (var i = 0; i < plugin.events.length; i++) events.addEvent(plugin.events[i], plugin[plugin.events[i]]);
+        for (var i = 0; i < plugin.flags.length; i++) events.addFlag(plugin.flags[i], plugin[plugin.flags[i]]);
+    });
+
+    // event handlers
+    bot.on('ready', events.ready);
+    bot.on('disconnected', events.disconnected);
+    //bot.on('warn', events.warn);
+    bot.on('error', events.error);
+    bot.on('debug', events.debug);
+    bot.on('message', events.message);
+
+    bot.loginWithToken(discord_token);
+} catch (e) {
+    console.log(e.stack.replace(/\s\s\s\s/g, '\r\n    '));
 }
-
-// create directories if they don't exist
-if (!fs.existsSync(config.fileDir)) fs.mkdirSync(config.fileDir);
-if (!fs.existsSync(config.logDir)) fs.mkdirSync(config.logdir);
-if (!fs.existsSync(config.serverDir)) fs.mkdirSync(config.serverDir);
-
-// load plugins
-fs.readdirSync(config.pluginDir).forEach(function (file) {
-    var plugin = require(config.pluginDir + file);
-    for (var i = 0; i < plugin.commands.length; i++) events.addCommand(plugin.commands[i], plugin[plugin.commands[i]]);
-    for (var i = 0; i < plugin.events.length; i++) events.addEvent(plugin.events[i], plugin[plugin.events[i]]);
-    for (var i = 0; i < plugin.flags.length; i++) events.addFlag(plugin.flags[i], plugin[plugin.flags[i]]);
-});
-
-// event handlers
-bot.on('ready', events.ready);
-bot.on('disconnected', events.disconnected);
-//bot.on('warn', events.warn);
-bot.on('error', events.error);
-bot.on('debug', events.debug);
-bot.on('message', events.message);
-
-bot.loginWithToken(discord_token);
