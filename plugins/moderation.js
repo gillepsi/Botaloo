8829614dc30db371e3cd97dc43a64f4687100020
Slypher <connorgill@live.com.au>
2016-08-31 14:25:36 +1000

    Fixed a major issue with clear command

--- plugins/moderation.js
+++ plugins/moderation.js
@@ -36,7 +36,7 @@ exports.mute = {
     description: 'mute a user',
     usage: '<username>',
     process: function (bot, msg, arg) {
-        if (!msg.server) return;
+        if (!msg.server) return bot.sendMessage(msg.channel, 'Nope! :poop:');
 
         var user = msg.channel.server.members.get('username', arg);
 
@@ -66,11 +66,11 @@ exports.clear = {
     process: function (bot, msg, arg) {
         if (!msg.server) return bot.sendMessage(msg.channel, 'Nope! :poop:');
         if (arg > 100) return bot.sendMessage(msg.channel, 'Nope! :poop:');
-        msg.channel.getLogs((arg != '' ? arg += 1 : arg = '2'), function (error, messages) {
+        msg.channel.getLogs((arg != '' ? arg = parseInt(arg) + 1 : arg = '2'), function (error, messages) {
             if (error) return bot.sendMessage(msg.channel, 'Error getting logs :cry:');
             bot.deleteMessages(messages, function (error) {
                 if (error) return bot.sendMessage(msg.channel, 'Error deleting messages :cry:');
-                bot.sendMessage(msg.channel, 'Deleted ' + messages.length + ' messages :ok_hand:');
+                bot.sendMessage(msg.channel, 'Deleted ' + messages.length - 1 + ' messages :ok_hand:');
             });
         });
     }
@@ -92,18 +92,17 @@ exports.ready = function (bot) {
 }
 
 exports.message = function (bot, message) {
-    if (message.server) {
-        if (exports.getMuted()[message.server.id][message.author.id]) {
-            message.delete(function (error) {
-                bot.sendMessage(msg.channel, 'Error deleting ' + message.author.username + '\'s message :cry:');
-            });
-        }
+    if (!message.server) return;
+    if (exports.getMuted()[message.server.id][message.author.id]) {
+        message.delete(function (error) {
+            bot.sendMessage(msg.channel, 'Error deleting ' + message.author.username + '\'s message :cry:');
+        });
+    }
 
-        if (exports.getMuted()[message.server.id][bot.user.id]) {
-            var list = exports.getMuted();
-            delete list[message.server.id][bot.user.id];
-            exports.setMuted(list);
-        }
+    if (exports.getMuted()[message.server.id][bot.user.id]) {
+        var list = exports.getMuted();
+        delete list[message.server.id][bot.user.id];
+        exports.setMuted(list);
     }
 }
 
@@ -111,6 +110,7 @@ exports.m = {
     bool: false,
     description: 'bot will not send a response',
     process: function (bot, msg, arg) {
+        if (!msg.server) return bot.sendMessage(msg.channel, 'Nope! :poop:');
         var list = exports.getMuted();
         list[msg.server.id][bot.user.id] = {
             id: bot.user.id,
